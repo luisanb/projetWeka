518f513f1b19b123d82877116656ae0011f29bd8
Sandra Jagodzińska <sandrajagodzinska@MacBook-Pro-Sandra.local>
2023-05-04 20:26:25 +0200

    petit modification

--- longueurMoyenne.py
+++ longueurMoyenne.py
@@ -6,7 +6,6 @@ def countLengthMoyenne():
 	"""cette fontction prend le chemin relative du corpus et compte la longueur moyenne de textes contenus dans ce corpus"""
 	doc = 0
 	length = 0
-	corpus = sys.argv[1] 
 	for file in glob.glob("corpus/*/*"): 
 		with open(file, 'r') as f:
 			text = f.read()
